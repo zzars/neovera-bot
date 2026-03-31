daaa31402dcb6231cb0cc1f777969b09c6a9a9f4
zzars <wibusoft909@gmail.com>
2026-03-31 21:22:54 +0700

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+import makeWAsocket, { useMultiFileAuthStste, fetchLatestWaWebVersion } from "baileys";
+
+const { state, saveCreds } = useMultiFileAuthStste('/auth')
+const { version } = await fetchLatestWaWebVersion()
+
+const sock = makeWAsocket({
+  version,
+  browser: Browsers.android("chrome"),
+  syncFullHistory: true,
+  printQRInTerminal: false
+});
+
+if (!sock.authState.creds.registered) {
+  const number = "62895802285200"
+  const code = await sock.requestPairingCode(number, "NEOVERAA")
+  console.log(code)
+}
+
+sock.ev.on("creds.update", saveCreds)
